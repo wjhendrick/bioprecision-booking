88f9839b50403f64269ea5982947a2e7247f508e
wjhendrick <wjhendrick@bioprecision.com>
2026-06-03 07:27:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ require('dotenv').config();
 const express    = require('express');
 const cors       = require('cors');
 // Square payments handled via direct HTTP API (axios)
+const SQUARE_API_URL = 'https://connect.squareup.com/v2';
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
